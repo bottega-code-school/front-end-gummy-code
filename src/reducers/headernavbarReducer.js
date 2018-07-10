579f9cb84e0f5f07df4d6cb6cd425d08570058e8
Max <maxnelson997@gmail.com>
2018-07-10 13:19:32 -0600

    connected header and navbar components to redux store to get links

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -4,8 +4,22 @@ import {
 } from '../actions/types';
 
 const INITIAL_STATE = {
-    headerLinks: [],
-    navbarLinks: []
+    headerLinks: [
+        {
+            title: 'yooo'
+        },
+        {
+            title: 'YOOO'
+        }
+    ],
+    navbarLinks: [
+        {
+            title: 'account'
+        },
+        {
+            title: 'purchases'
+        }
+    ]
 }
 
 export default function(state = INITIAL_STATE, action) {
