1c40a8325a5651d3ec08cb0ca68da26ba365b746
Max <maxnelson997@gmail.com>
2018-07-10 10:53:58 -0600

    styled formfields components

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -21,6 +21,7 @@ class SignInForm extends Component {
                 placeholder='Password'
                 name='password'
                 component={FormInput}/>
+                <div className='sign-in-form__line'></div>
                 <Field className='sign-in-form__login'
                 onClick={() => console.log('tryna submit')}
                 type='submit'
