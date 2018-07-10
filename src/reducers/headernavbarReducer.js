0994ff343586e309c91049b84c74f4220db87ebe
Max <maxnelson997@gmail.com>
2018-07-10 14:56:02 -0600

    finished navbar feature

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -1,6 +1,7 @@
 import {
     SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    CHANGE_NAVBAR_ACTIVE
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -19,6 +20,18 @@ export default function(state = INITIAL_STATE, action) {
             return {
                 ...state,
                 navbarLinks: action.payload
+            }
+        case CHANGE_NAVBAR_ACTIVE: 
+            const navbarLinks = state.navbarLinks.map(link => {
+                link.active = false;
+                if(link._id == action.payload) {
+                    link.active = true;
+                }
+                return link;
+            })
+            return {
+                ...state,
+                navbarLinks
             }
         default: return state;
     }
