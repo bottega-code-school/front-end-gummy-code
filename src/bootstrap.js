096ace55032385154a91eeacec57bdaf7c604e94
Max <maxnelson997@gmail.com>
2018-07-09 19:17:09 -0600

    setup reduxform

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, Switch, Route } from 'react-router-dom';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
 import './style/main.scss';
 
@@ -20,9 +20,11 @@ function main() {
     <Provider store={createStoreWithMiddleware(reducers)}>
       <Router history={history}>
         <Layout>
-            <Switch>
-              <Route path='/' exact component={Signin}/>
-            </Switch>
+          <Switch>
+            <Route path='/' exact component={Signin}/>
+            <Route path='/signin' exact component={Signin}/>
+            <Route path='/signup' exact component={Signup}/>
+          </Switch>
         </Layout>
       </Router>
     </Provider>
