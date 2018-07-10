def239c86fb80c1880d1e3e0221c0bf904296699
Max <maxnelson997@gmail.com>
2018-07-10 15:05:59 -0600

    finished header feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -10,6 +10,19 @@ class Account extends Component {
 
     componentDidMount() {
 
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Shop',
+                path: '/shop'
+            },
+            {
+                _id: 1,
+                title: 'Logout',
+                path: '/'
+            }
+        ]
+
         const navbarLinks = [
             {
                 _id: 0,
@@ -25,7 +38,7 @@ class Account extends Component {
             }
         ]
 
-        this.props.setHeaderLinks([]);
+        this.props.setHeaderLinks(headerLinks);
         this.props.setNavbarLinks(navbarLinks);
     }
 
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 import SignUpForm from './signupForm';
 import PageTitle from '../pageTitle';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 class SignUp extends Component {
+
+    componentDidMount() {
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([]);
+    }
 
     onSubmit = (fields) => {
         console.log(fields);
@@ -18,5 +26,7 @@ class SignUp extends Component {
         )
     }
 }
+
+SignUp = connect(null, actions)(SignUp);
 
 export default SignUp;
