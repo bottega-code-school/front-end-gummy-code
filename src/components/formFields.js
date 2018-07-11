1cba28827c16e311883e9e7037adc139f1aef4dd
Max <maxnelson997@gmail.com>
2018-07-11 11:52:54 -0600

    built long gray button component and placed instance of it on account information form to create change password button

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -28,3 +28,22 @@ export class FormButton extends Component {
         )
     }
 }
+
+
+export class LongGrayButton extends Component {
+    render() {
+        const { className, labelTitle, title, type, onClick, input } = this.props;
+        return (
+            <div className={`${className} form-button-long-gray`}>
+                <label className='form-button-long-gray__label'>{labelTitle}</label>
+                <button className={`form-button-long-gray__button`}
+                        type={type}
+                        {...input}
+                        onClick={onClick}
+                >
+                {title}
+                </button>
+            </div>
+        )
+    }
+}
