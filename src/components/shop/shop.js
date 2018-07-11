5cf4f50935db584a739f79fda44fc8bd5e5b52b4
Max <maxnelson997@gmail.com>
2018-07-11 15:43:39 -0600

    filtered products feature complete

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -25,7 +25,6 @@ class Shop extends Component {
             this.props.setNavbarLinks(nextProps.categories, (_id) => this.props.filterProductsWithCategoryId(_id));
         }
         return true
-
     }
 
     render() {
@@ -33,7 +32,22 @@ class Shop extends Component {
         return (
             <div className='shop'>
                 {/* shop search bar */}
-                {/* shop product */}
+                <div className='shop__products'>
+                    {
+                        this.props.filteredProducts.map(product => {
+                            return (
+                                <div key={product._id} className='shop-product'>
+                                    <div className='shop-product__title'>
+                                        {product.title}
+                                    </div>
+                                    <div className='shop-product__description'>
+                                        {product.description}
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
                 {/* shop cart button */}
             </div>
         )
@@ -41,9 +55,10 @@ class Shop extends Component {
 }
 
 function mapStateToProps(state) {
-    const { categories } = state.shop;
+    const { categories, filteredProducts } = state.shop;
     return {
-        categories
+        categories,
+        filteredProducts
     } 
 }
 
--- src/reducers/shopReducer.js
+++ src/reducers/shopReducer.js
@@ -6,8 +6,8 @@ import {
 
 const INITIAL_STATE = {
     categories: [],
-    productsSelected: [],
-    products: []
+    products: [],
+    filteredProducts: []
 }
 
 export default function(state = INITIAL_STATE, action) {
@@ -24,10 +24,15 @@ export default function(state = INITIAL_STATE, action) {
                 products: action.payload
             }
         case FILTER_PRODUCTS_WITH_CATEGORY_ID:
-            console.log(action.payload);
+            var filteredProducts = [];
+            state.products.map(product => {
+                if(product.belongsTo.includes(action.payload)) {
+                    filteredProducts.push(product);
+                }
+            })
             return {
                 ...state,
-                // selectedCategoryId
+                filteredProducts
             }
         default: return state;
     }
