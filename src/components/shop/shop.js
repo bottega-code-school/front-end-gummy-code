a4c990deedf2bf21c49ed36338e2f297e733844e
Max <maxnelson997@gmail.com>
2018-07-11 17:00:55 -0600

    built green price tag component and quantity component and added remaining jsx to product component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
+import ShopProduct from './shopProduct';
 
 class Shop extends Component {
 
@@ -41,14 +42,7 @@ class Shop extends Component {
                     {
                         this.props.filteredProducts.map(product => {
                             return (
-                                <div key={product._id} className='shop-product'>
-                                    <div className='shop-product__title'>
-                                        {product.title}
-                                    </div>
-                                    <div className='shop-product__description'>
-                                        {product.description}
-                                    </div>
-                                </div>
+                                <ShopProduct {...product} key={product._id} />
                             )
                         })
                     }
