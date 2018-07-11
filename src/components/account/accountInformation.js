294ef598302bad05e22c714cb2d6ad63e0fb2574
Max <maxnelson997@gmail.com>
2018-07-11 10:47:39 -0600

    built account information form from signinform as a template

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+import PageTitle from '../pageTitle';
+import AccountInformationForm from './accountInformationForm';
+
 class AccountInformation extends Component {
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
-            <div>
-                account info
+            <div className='account-information'>
+                <PageTitle className='account-information' title='Account Information'/>
+                <AccountInformationForm onSubmit={this.onSubmit} className='account-information__form' />
             </div>
         )
     }
