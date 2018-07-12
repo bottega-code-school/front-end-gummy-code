b147d75b82d55ac6d38d813c01fdb2f0323041d5
Max <maxnelson997@gmail.com>
2018-07-12 17:12:39 -0600

    finished styling cart

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -43,8 +43,7 @@ class Shop extends Component {
     }
 
     render() {
-        return <ShopCart className='shop__cart'/>
-
+        // return <ShopCart className='shop__cart'/>
         return (
             <div className='shop'>
                 <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
