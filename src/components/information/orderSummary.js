c42b6c6f6b4357f7bf52be97ca43eb812e5091e2
Max <maxnelson997@gmail.com>
2018-07-13 16:14:03 -0600

    built order summary component, infotitle and underlined title components

---
+++ src/components/information/orderSummary.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import { UnderlinedTitle, InfoTitle } from './infoHelp';
+
+class OrderSummary extends Component {
+    render() {
+        const { className } = this.props;
+        return (
+            <div className={`${className} order-summary`}>
+                <UnderlinedTitle className='order-summary__title'/>
+                <InfoTitle className='order-summary__subtotal' title='4 stickers' value='$7.96'/>
+                <InfoTitle className='order-summary__tax-shipping' title='Taxes & Shipping' value='$0.00'/>
+                <InfoTitle className='order-summary__total' title='Total' value='$8.02'/>
+            </div>
+        )
+    }
+}
+
+export default OrderSummary;
--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -6,6 +6,8 @@ import { FormInput, FormButton } from '../formFields';
 
 import history from '../../history';
 
+import OrderSummary from './orderSummary';
+
 class ShippingForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
@@ -58,7 +60,7 @@ class ShippingForm extends Component {
                 name='back'
                 short={true}
                 component={FormButton}/>
- 
+                <OrderSummary className='shipping-form__summary'/>
             </form>
         )
     }
