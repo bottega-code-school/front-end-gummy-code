05ff753bae68efed60beaab595f0eeec26f0e351
Max <maxnelson997@gmail.com>
2018-07-13 10:57:18 -0600

    built review form buttons

--- src/components/order/review.js
+++ src/components/order/review.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
 import PageTitle from '../pageTitle';
+import ReviewForm from './reviewForm';
 
 class Review extends Component {
 
@@ -12,10 +13,15 @@ class Review extends Component {
         this.props.setNavbarLinks([]);
     }
 
+    onSubmit = (fields) => {
+        console.log('fields');
+    } 
+
     render() {
         return (
             <div className='review'>
                 <PageTitle className='review__page-title' title='Order Review'/>
+                <ReviewForm className='review__form' onSubmit={this.onSubmit}/>
             </div>
         )
     }
---
+++ src/components/order/reviewForm.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import { reduxForm, Field } from 'redux-form';
+
+import { FormButton } from '../formFields';
+
+import history from '../../history';
+
+class ReviewForm extends Component {
+    render() {
+        const { className, handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit} className={`${className} review-form`}>
+                <div className='review-form__line'></div>
+                <Field className='review-form__proceed'
+                onClick={() => history.push('/account')}
+                type='submit'
+                title='Proceed to Checkout'
+                name='proceed'
+                component={FormButton}/>
+                <Field className='review-form__back'
+                onClick={() => history.push('/signin')}
+                type='button'
+                title='Back'
+                name='back'
+                short={true}
+                component={FormButton}/>
+            </form>
+        )
+    }
+}
+
+ReviewForm = reduxForm({
+    form: 'ReviewForm'
+})(ReviewForm);
+
+export default ReviewForm;
